154d3f0511618e961ee5e5f0dec50f5a0a7f7a04
khaledyouseff <khaledyoussouph36@gmail.com>
2024-03-14 06:54:06 +0200

    adding first bad scenario

--- GET_task/src/test/java/signUpTest/badSenariosSignUp.java
+++ GET_task/src/test/java/signUpTest/badSenariosSignUp.java
@@ -1,10 +1,9 @@
 package signUpTest;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.signUpPage;
 import pages.successfulSignUpPage;
@@ -14,8 +13,7 @@ import java.time.Duration;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class signUpTest extends baseTest.baseTest {
-
+public class badSenariosSignUp extends baseTest.baseTest {
     @Test
     public void successfulSignUPTest() throws InterruptedException {
 
@@ -23,41 +21,34 @@ public class signUpTest extends baseTest.baseTest {
 
         signUpPage signUpPage = new signUpPage(driver);
         successfulSignUpPage successfulSignUpPage = new successfulSignUpPage(driver);
-        signUpPage.setFirstName("Khaled");
+        //NOT starting with capital letter
+        signUpPage.setFirstName("khaled");
+
+        
         signUpPage.setLastName("yousef");
         signUpPage.setEmail("khaledYoussouph36@gmail.com");
         signUpPage.setPhone("01140342431");
         signUpPage.scrollDown();
 
 
-        String option ="Selenium Automation";
+        String option = "Selenium Automation";
         signUpPage.selectCourseDropDownElement(option);
         var selectedOption = signUpPage.getSelectedCourseOption();
-        assertEquals(selectedOption.size(), 1 , "incorrect number of selections");
-        assertTrue(selectedOption.contains(option),"incorrect option");
+        assertEquals(selectedOption.size(), 1, "incorrect number of selections");
+        assertTrue(selectedOption.contains(option), "incorrect option");
 
-        String option1 ="January";
+        String option1 = "January";
         signUpPage.selectMonthDropDownElement(option1);
         var selectedOption1 = signUpPage.getSelectedMonthOption();
-        assertEquals(selectedOption.size(), 1 , "incorrect number of selections");
-        assertTrue(selectedOption1.contains(option1),"incorrect option");
+        assertEquals(selectedOption.size(), 1, "incorrect number of selections");
+        assertTrue(selectedOption1.contains(option1), "incorrect option");
         signUpPage.checkHowDidYouKnowAboutUs();
         signUpPage.scrollDown();
         signUpPage.clickRegister();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div[2]/div/main/article/div/div[1]/div/div[1]/p")));
-        String successfulRegistrationMessage=successfulSignUpPage.findText();
+        String successfulRegistrationMessage = successfulSignUpPage.findText();
         assertEquals(successfulRegistrationMessage, "Your registration is completed. We will contact with you soon. Thank you !", "invalid");
 
 
-
-
-
-
-
-
-
-
-
     }
-
 }
